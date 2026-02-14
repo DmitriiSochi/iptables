c87b2c6afb88ffb2f2a62f8eb9410431b75b2f86
DmitriiSochi <87702904+DmitriiSochi@users.noreply.github.com>
2026-02-14 08:28:53 +0300

    правила для порта 5300 добавлены
    iкоторый буду использовать для виртуальной машины
    
    Co-authored-by: DmitriiSochi <bubenindk@gmail.com>

--- iptables_for_server.sh
+++ iptables_for_server.sh
@@ -56,4 +56,5 @@ iptables -A FI -p tcp --dport 443 -m recent --set --name HTTP_LIMIT
 iptables -A FI -p tcp --dport 443 -m recent --update --seconds 60 --hitcount 200 --name HTTP_LIMIT -j DROP
 iptables -A FI -p tcp --dport 443 -j ACCEPT
 #Настройка порта http/80 и 8080 от многочисленных ссесий 
-#Додавляю новые правила для 22 порта
+#Додавляю новые правила для 5300 порта, буду использовать для виртуальной машины
+iptables -A FI -p tcp -dport 5300 -j ACCEPT
